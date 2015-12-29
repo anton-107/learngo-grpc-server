320c323355d59e2d8902474c2bcf1f5543ac741a
Anton Nekipelov <anton.nekipelov@gmail.com>
2015-12-29 13:36:22 +0100

    add rest server

--- main.go
+++ main.go
@@ -1,18 +1,57 @@
 package main
 
 import (
+	"flag"
 	"github.com/anton-107/learngo-grpc-server/proto"
+	"github.com/gengo/grpc-gateway/runtime"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"net/http"
 )
 
-func main() {
-	listener, err := net.Listen("tcp", ":50051")
+const rpcAddress = ":50051"
+const restAddress = ":3001"
+
+func startRpcServer() {
+	listener, err := net.Listen("tcp", rpcAddress)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
+
 	helloworld.RegisterGreeterServer(grpcServer, &server{})
-	grpcServer.Serve(listener)
+	go grpcServer.Serve(listener)
+
+	log.Println("Started grpc service at " + rpcAddress)
+
+}
+
+func startRestServer() {
+	echoEndpoint := flag.String("greeter_endpoint", rpcAddress, "greeter endpoint")
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux()
+
+	err := helloworld.RegisterGreeterHandlerFromEndpoint(ctx, mux, *echoEndpoint)
+	if err != nil {
+		if err != nil {
+			log.Fatalf("failed to register a handler: %v", err)
+		}
+	}
+
+	http.ListenAndServe(restAddress, mux)
+	log.Println("Started rest service at " + restAddress)
+}
+
+func main() {
+	log.Println("Start RPC")
+	startRpcServer()
+	log.Println("Start Rest")
+	startRestServer()
+
 }
--- proto/generate.sh
+++ proto/generate.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
-protoc -I ./ ./helloworld.proto --go_out=plugins=grpc:./
+# proto:
+#protoc -I ./ ./helloworld.proto --go_out=plugins=grpc:./
+
+# Generate gRPC stub:
+protoc -I/usr/local/include -I. \
+ -I$GOPATH/src \
+ -I$GOPATH/src/github.com/gengo/grpc-gateway/third_party/googleapis \
+ --go_out=Mgoogle/api/annotations.proto=github.com/gengo/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:. \
+ ./helloworld.proto
+
+# Generate reverse-proxy:
+protoc -I/usr/local/include -I. \
+ -I$GOPATH/src \
+ -I$GOPATH/src/github.com/gengo/grpc-gateway/third_party/googleapis \
+ --grpc-gateway_out=logtostderr=true:. \
+ ./helloworld.proto
